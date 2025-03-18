45d68126ed4f8e468a0f85db38d5281f95c0189e
test java project <danilair@icloud.com>
2025-03-18 13:51:03 +0300

    test1

--- src/org/stepic/java/example/Main.java
+++ src/org/stepic/java/example/Main.java
@@ -15,7 +15,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
-
+       int i = 1;
+       int у2 = 2;
+       int c2 = 2;
     }
 
 }
