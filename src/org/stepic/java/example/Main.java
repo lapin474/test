71b43e4c38993076383ad44da453a662f899d2b4
test java project <danilair@icloud.com>
2025-03-18 13:51:29 +0300

    test2

--- src/org/stepic/java/example/Main.java
+++ src/org/stepic/java/example/Main.java
@@ -1,18 +1,5 @@
 package org.stepic.java.example;
 
-import java.math.*;
-import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
        int i = 1;
